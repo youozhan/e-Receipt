260a6ece971bc9c684d5c6014fde03a0906d819e
Youchun <30670835+youozhan@users.noreply.github.com>
2019-04-01 23:21:39 -0400

    Support multiple row of data

--- chrome_extension_print/scripts/receipt.js
+++ chrome_extension_print/scripts/receipt.js
@@ -1,34 +1,53 @@
-$(document).ready(function () {
-    chrome.storage.local.get(['purchaseStorageKey'], function (result) {
+$(document).ready(() => {
+    chrome.storage.local.get(['purchaseStorageKey'], (result) => {
 
         var purchaseData = JSON.parse(result['purchaseStorageKey'])[0]
 
-        $('#pname').text($('#pname').text() + purchaseData.name)
-        $('#pprice').text($('#pprice').text() + purchaseData.price)
+        if (purchaseData) {
 
-        console.log("purchase data get")
-        // console.log(purchaseData.name)
+            $('#pname').text($('#pname').text() + purchaseData.name)
+            $('#pprice').text($('#pprice').text() + purchaseData.price)
+
+            console.log("purchase data get")
+            // console.log(purchaseData.name)
+        }
 
     })
 
-    chrome.storage.local.get(['subscriptionStorageKey'], function (result) {
+    chrome.storage.local.get(['subscriptionStorageKey'], (result) => {
 
-        var subscriptionData = JSON.parse(result['subscriptionStorageKey'])[0]
-        $('#sname').text($('#sname').text() + subscriptionData.name)
+        var subscriptionData = JSON.parse(result['subscriptionStorageKey'])
+
+        var items = []
+
+        subscriptionData.forEach((item) => {
+            items.push(`<tr><td>${item.name}</td><td>${(item.id)[0]}</td></tr>`)
+        })
+
+        $('table#subscriptionlist').append(items.join(''))
+
         console.log("subscription data get")
 
     })
 
-    chrome.storage.local.get(['settingStorageKey'], function (result) {
+    chrome.storage.local.get(['settingStorageKey'], (result) => {
 
         var settingData = JSON.parse(result['settingStorageKey'])
-        $('#setname').text($('#setname').text() + settingData)
+
+        var items = []
+
+        Object.keys(settingData).forEach((key) => {
+            items.push(`<li>${key}: ${settingData[key]}</li>`)
+        })
+
+        $('div#settinglist ul').append(items.join(''))
+
         console.log("setting data get")
         console.log(JSON.parse(result['settingStorageKey']))
 
     })
 
-    chrome.storage.local.get(['adsStorageKey'], function (result) {
+    chrome.storage.local.get(['adsStorageKey'], (result) => {
 
         var adsData = JSON.parse(result['adsStorageKey'])[0]
         $('#aname').text($('#aname').text() + adsData)
