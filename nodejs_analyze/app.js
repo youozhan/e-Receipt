da04c63f1d46e38b84fba01ebfe4bdf71953dccf
Youchun <30670835+youozhan@users.noreply.github.com>
2019-04-07 21:51:10 -0400

    Collect three metrics from file

--- nodejs_analyze/app.js
+++ nodejs_analyze/app.js
@@ -1,14 +1,29 @@
-const fs = require('fs')
-
-let rawdata = fs.readFileSync('data/ReceiptData.json')
-let profile = JSON.parse(rawdata)
+const glob = require('glob'),
+    fs = require('fs')
 
 let settingsOnAmount = 0
+let subscriptionOnAmount = 0
+let adOnAmount = 0
 
-Object.keys(profile.settingStorageKey).forEach(key => {
-    if (profile.settingStorageKey[key] === "On") {
-        settingsOnAmount += 1
-    }
+glob(__dirname + '/data/*.json', {}, (err, files)=>{
+    files.forEach(file => {
+        
+        let rawdata = fs.readFileSync(file)
+        let profile = JSON.parse(rawdata)
+
+        Object.keys(profile.settingStorageKey).forEach(key => {
+            if (profile.settingStorageKey[key] === "On") {
+                settingsOnAmount += 1
+            }
+        })
+
+        subscriptionOnAmount += profile.subscriptionStorageKey.length
+        adOnAmount += profile.adsStorageKey.length
+
+    })
+
+    console.log(files.length)
+    console.log("setting total " + settingsOnAmount)
+    console.log("subscription total " + subscriptionOnAmount)
+    console.log("ad total " + adOnAmount)
 })
-
-console.log(settingsOnAmount)
