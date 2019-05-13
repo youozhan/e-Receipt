41e6c99153410f81daa94bee60b06f15dd35fa74
Youchun <30670835+youozhan@users.noreply.github.com>
2019-05-12 23:51:35 -0400

    Change made for AWs

--- nodejs_analyze/app.js
+++ nodejs_analyze/app.js
@@ -92,8 +92,8 @@ let analyzeResult = []
 let profileCount = 0
 
 // const folderpath = '/Users/yz/Documents/Processing/sketch_190328a/'
-const folderpath = '/Users/yz/Documents/GitHub/chrome-extension-youtube-analytics/p5/assets/'
-// const folderpath = '/Users/yhw/Codes/github.com/hidden-space/p5/assets/'
+// const folderpath = '/Users/yz/Documents/GitHub/chrome-extension-youtube-analytics/p5/assets/'
+const folderpath = '/Users/yhw/Codes/github.com/hidden-space/p5/assets/'
 
 let obj = {
     planets: []
